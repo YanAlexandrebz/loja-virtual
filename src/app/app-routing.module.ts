4bc0e91d77020c1b7fcaf4f604400bd5ee181453
Yan Alexandre <rm77740@fiap.com.br>
2021-08-25 17:54:07 -0300

    Adicionado tela inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { CarrinhoComponent } from './components/carrinho/carrinho.component';
+import { ProdutoComponent } from './components/produto/produto.component';
+import { InicialComponent } from './components/inicial/inicial.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {path: '', component: InicialComponent},
+  {path: 'produto/:id', component: ProdutoComponent},
+  {path: 'carrinho', component: CarrinhoComponent},
+  {path: 'checkout', component: CheckoutComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/inicial/inicial.component.ts
@@ -0,0 +1,21 @@
+import { ProdutoService } from './../../services/produto.service';
+import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/app/models/produto.model';
+
+@Component({
+  selector: 'app-inicial',
+  templateUrl: './inicial.component.html',
+  styleUrls: ['./inicial.component.css']
+})
+export class InicialComponent implements OnInit {
+
+  produtos: Produto[] = [];
+
+  constructor(private produtoService: ProdutoService) { }
+
+  ngOnInit(): void {
+    this.produtos = this.produtoService.listarTodos();
+  }
+
+
+}
